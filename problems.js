0996588ff3d38d9b133cc5f73d887a4ca68bca00
abdulhadi318 <abdulhadiqureshi35@gmail.com>
2023-05-30 12:39:35 +0500

    ecomerece site

---
+++ problems.js
@@ -0,0 +1,48 @@
+// find the heighest integar in array
+// let myArray = [1, 5, 6, 8, 9, 4, 5, 12, 11]
+// let maxIntegar = 0
+// function findMax() {
+//     for (let i = 0; i < myArray.length; i++) {
+//         let currentInegar = myArray[i]
+//         if (currentInegar > maxIntegar) {
+//             maxIntegar = currentInegar
+//         }
+//     }
+// }
+// findMax()
+
+
+// Update the user details
+// STEPS
+// if user want to edit values
+// YES:
+// ask the key
+// ask the updatedValue
+// find the key
+// update the key as per updatedValue
+// console the new result
+
+// No:
+// console old values
+
+
+// let user = {
+//     name: "Aryan",
+//     email: "aryan@banoqabil.com",
+//     password: "pass1234"
+// }
+
+// function updateUser() {
+//     let ifEdit = prompt("Are you want the update")
+//     if (ifEdit.toLowerCase() === "yes") {
+//         let updatingKey = prompt("Enter the field")
+//         let updatnigValue = prompt("Enter the Value")
+//         user[updatingKey] = updatnigValue
+//         console.log(user)
+//     } else if (ifEdit.toLowerCase() === "no") {
+//         console.log(user)
+//     }
+// }
+
+// updateUser()
+
